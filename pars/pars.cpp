efb590fade5a226835bc0931ef875fff5faec80d
Amin Asadi <aminasadiam@outlook.com>
2020-11-08 18:36:22 +0330

    Sorting the code

--- pars/pars.cpp
+++ pars/pars.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <string>
 #include <cstring>
+#include <fstream>
 
 
 #include "global_helpers.cpp"
@@ -13,9 +14,8 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    // cout << "there are " << argc << " arguments : " << argv[1] << endl;
-
     string filename = "";
+    // todo: debug argv size of
     if (sizeof(argv[1]) >= 2)
     {
         filename = argv[1];
@@ -23,43 +23,17 @@ int main(int argc, char *argv[])
         strcpy(Cfilename, filename.c_str());
 
         char *token = strtok(Cfilename, ".pars");
-        if (token != NULL)
+        if (token == NULL)
         {       
             error("Incorrect file extension", 1);
         }
         
+        cout << Cfilename << endl;
     }
     else
     {
         error("Please provide simc file path", -1);
     }
-    
-    
-    
-    cout << filename << endl;
 
     return 0;
 }
-
-// int run(char** argv)
-// {
-//     string filename = "";
-
-//     if(sizeof(argv) >= 2)
-//     {
-//         filename = argv[1];
-//         char Cfilename[filename.size() + 1];
-//         strcpy(Cfilename, filename.c_str());
-
-//         char *token = strtok(Cfilename, ".pars");
-//         if(token == NULL)
-//         {
-//             error("Incorrect file extension", 1);
-//         }
-//     }
-//     else
-//     {
-//         error("Please provide simc file path", -1);
-//     }
-//     return 0;
-// }
