6af520a4d1a01b63ebca3babc219bd35053b966d
Amin Asadi <aminasadiam@outlook.com>
2020-11-09 13:05:50 +0330

    debug getline

--- pars/op_codes.cpp
+++ pars/op_codes.cpp
@@ -12,3 +12,20 @@ int createCppCode(string cppFilename, string code)
 
     return 0;
 }
+
+string readParsFile(string filename)
+{
+    string result = "";
+    ifstream infile(filename);
+    if(infile.is_open())
+    {
+        string line;
+        while (getline(infile, line, ' '))
+        {
+            result += line.c_str();
+        }
+        infile.close();
+    }
+
+    return result;
+}
