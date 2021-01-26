afe9202712093302ffd56aad9f1caef77673616d
Amin Asadi <aminasadiam@outlook.com>
2021-01-26 18:58:59 +0330

    edit color codes

--- pars/global_helpers.cpp
+++ pars/global_helpers.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 void error(string message, int line_number)
 {
-    cout << "\x1B[31m[Line : " << line_number << "\x1B[31m]\033[0m" << "\x1B[33m Error : " << message << endl;
+    cout << "\x1B[31m[Line : " << line_number << "\x1B[31m]\033[0m" << "\x1B[33m Error : " << message << "\033[0m" << endl;
     exit(0);
 }
 
 void success(string message, string cppFilename)
 {
-    cout << "\033[92m[ SUCCESS ] " << message << cppFilename << endl;
+    cout << "\033[92m[ SUCCESS ] \033[0m" << message << "\x1B[33m" << cppFilename << "\033[0m" << endl;
     exit(0);
 }
